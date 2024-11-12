f44a6ccdc60b9f455f595e57bf91a0816d590e15
boney <boney.fernandez@metaschool.so>
2024-11-12 19:37:56 +0530

    fix

--- contract/src/msg.rs
+++ contract/src/msg.rs
@@ -3,7 +3,7 @@ use cosmwasm_schema::{cw_serde, QueryResponses};
 
 #[cw_serde]
 pub struct InstantiateMsg {
-    pub owner: Option<String>,
+    pub owner: String,
 }
 
 #[cw_serde]
@@ -30,14 +30,13 @@ pub enum ExecuteMsg {
 #[derive(QueryResponses)]
 pub enum QueryMsg {
     #[returns(ListResponse)]
-    QueryUserList { 
-        user: String, 
-        start_after: Option<u64>, 
-        limit: Option<u32> 
+    QueryUserList {
+        user: String,
+        start_after: Option<u64>,
+        limit: Option<u32>,
     },
 }
 
-// We define a custom struct for each query response
 #[cw_serde]
 pub struct ListResponse {
     pub entries: Vec<Entry>,
