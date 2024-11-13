7da2b95fb35294afa367fe0d98b72cf34e0179bb
boney <boney.fernandez@metaschool.so>
2024-11-13 11:17:57 +0530

    cosm-wasm version bump

--- contract/src/contract.rs
+++ contract/src/contract.rs
@@ -1,17 +1,16 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{
-    to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Order, Response, StdResult,
+    to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Order, Response, StdResult,
 };
 use cw2::set_contract_version;
 use cw_storage_plus::Bound;
 use std::ops::Add;
 
 use crate::error::ContractError;
-use crate::msg::{ ExecuteMsg, InstantiateMsg, ListResponse, QueryMsg};
+use crate::msg::{ExecuteMsg, InstantiateMsg, ListResponse, QueryMsg};
 use crate::state::{Entry, Priority, Status, ENTRY_SEQ, LIST};
 
-// version info for migration
 const CONTRACT_NAME: &str = "crates.io:cw-to-do-list";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
 
@@ -20,14 +19,14 @@ pub fn instantiate(
     deps: DepsMut,
     _env: Env,
     _info: MessageInfo,
-    _msg: InstantiateMsg,
+    msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
+    let validated_owner = deps.api.addr_validate(&msg.owner)?;
     set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
-
     ENTRY_SEQ.save(deps.storage, &0u64)?;
-
-    Ok(Response::new()
-        .add_attribute("method", "instantiate"))
+    Ok(Response::default()
+        .add_attribute("action", "instantiate")
+        .add_attribute("owner", validated_owner.as_str()))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -48,12 +47,11 @@ pub fn execute(
             description,
             status,
             priority,
-            owner
+            owner,
         } => execute_update_entry(deps, info, id, description, status, priority, owner),
         ExecuteMsg::DeleteEntry { id, owner } => execute_delete_entry(deps, info, id, owner),
     }
 }
-
 pub fn execute_create_new_entry(
     deps: DepsMut,
     _info: MessageInfo,
@@ -78,20 +76,26 @@ pub fn execute_create_new_entry(
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
     match msg {
-        QueryMsg::QueryUserList { user, start_after, limit } => {
-            to_binary(&query_user_list(deps, user, start_after, limit)?)
-        }
+        QueryMsg::QueryUserList {
+            user,
+            start_after,
+            limit,
+        } => to_json_binary(&query_user_list(deps, user, start_after, limit)?),
     }
 }
 
-// Limits for pagination
 const MAX_LIMIT: u32 = 30;
 const DEFAULT_LIMIT: u32 = 10;
 
-pub fn query_user_list(deps: Deps, user: String, start_after: Option<u64>, limit: Option<u32>) -> StdResult<ListResponse> {
+pub fn query_user_list(
+    deps: Deps,
+    user: String,
+    start_after: Option<u64>,
+    limit: Option<u32>,
+) -> StdResult<ListResponse> {
     let limit = limit.unwrap_or(DEFAULT_LIMIT).min(MAX_LIMIT) as usize;
     let start = start_after.map(Bound::exclusive);
-    
+
     let entries: StdResult<Vec<_>> = LIST
         .range(deps.storage, start, None, Order::Ascending)
         .filter(|item| {
